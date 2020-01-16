1ae51aaef26e398ecdc034491f74fa52e3e9e778
Adam Meszaros <ameszaros@lambda-x.com>
2020-01-16 21:08:26 +0100

    Initiall commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# Ultimate-Guitar Tab API :guitar:
+
+
+![Python-Version](https://img.shields.io/badge/Python-3.7.4-blue.svg)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ugapi',
+    version='0.1',
+    description='Ultimate-Guitar Tab API',
+    url='http://github.com/doodoroma/ugapi',
+    author='Adam Meszaros',
+    author_email='doodoroma@gmail.com',
+    license='MIT',
+    packages=['ugapi'],
+    zip_safe=False
+)
