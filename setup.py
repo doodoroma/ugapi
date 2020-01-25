8154a651dd9ab9fd960afff7dd4f2edfea92cc7d
Adam Meszaros <ameszaros@lambda-x.com>
2020-01-25 12:03:54 +0100

    g

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1',
     description='Ultimate-Guitar Tab API',
     url='http://github.com/doodoroma/ugapi',
-    download_url='https://github.com/doodoroma/ugapi/archive/0.1.0.tar.gz',
     author='Adam Meszaros',
     author_email='doodoroma@gmail.com',
     license='MIT',
